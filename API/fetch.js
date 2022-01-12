731aa8529626ce82257d3a801aec96f1aa9943cd
lharrak-botaina <lharrak.botaina@gmail.com>
2022-01-12 02:10:19 -0800

    add await fetch

--- API/fetch.js
+++ API/fetch.js
@@ -1,4 +1,23 @@
+//  async  function get(){
+// const reponce = await fetch('https://api.openweathermap.org/data/2.5/weather?q=tanger&appid=1253309e30b4fb953c136c1426565be0')
+// const data = await reponce.json()
+
+// console.log(data)
+
+// }
+// get()
+
+
+
+
+
+
+
 fetch('https://api.openweathermap.org/data/2.5/weather?q=tanger&appid=1253309e30b4fb953c136c1426565be0')
+
+
+
+
 .then (function(reponse){
     return reponse.json()
 })
