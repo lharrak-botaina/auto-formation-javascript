ee8089cd6732717e76a1b54d3e5705360f16134d
lharrak-botaina <lharrak.botaina@gmail.com>
2022-01-12 01:53:11 -0800

    add url

--- API/fetch.js
+++ API/fetch.js
@@ -1,1 +1,8 @@
-fetch('https://api.thecatapi.com/v1/images/search')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=tanger&appid=1253309e30b4fb953c136c1426565be0')
+.then (function(reponse){
+    return reponse.json()
+})
+
+.then(function(data){
+    console.log(data)
+})
